92ecfeb432f963346e62deed53217886f3896f5f
Nguyen Thanh Luan <luann4099@gmail.com>
2022-12-02 10:14:03 +0700

    thêm form add user

--- routes/be.php
+++ routes/be.php
@@ -1,7 +1,15 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+
 Route::prefix("admin")->group(function () {
     Route::get("/dashboard", function () {
         return view("backend.layout");
     })->name('admin.dashboard');
+
+    Route::prefix("user")->group(function () {
+        Route::get('/add', function () {
+            return view('backend.user.add');
+        })->name('admin.user.add');
+    });
 });
