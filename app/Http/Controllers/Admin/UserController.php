f50918968a719bf665b35156482e27e7cae7f0f8
Nguyen Thanh Luan <luann4099@gmail.com>
2022-12-02 10:46:19 +0700

    add user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,8 +14,38 @@ class UserController extends Controller
     {
         return view('backend.user.add');
     }
-    public function doAdd()
+    public function doAdd(Request $request)
     {
-
+        $request->validate([
+            'name' => 'required|unique:users,name',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'level' => 'required',
+            'address' => 'required',
+            'phone' => 'required|numeric|min:10',
+        ], [
+            'name.required' => 'Name is required',
+            'email.required' => 'Email is required',
+            'name.unique' => 'Name is already exists',
+            'email.email' => 'Email is invalid',
+            'email.unique' => 'Email is already exists',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 6 characters',
+            'level.required' => 'Level is required',
+            'address.required' => 'Address is required',
+            'phone.required' => 'Phone is required',
+            'phone.min' => 'Phone must be at least 10 characters',
+            'phone.numeric' => 'Phone is invalid',
+        ]);
+        //dd($request->all());
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->level = $request->level;
+        $user->address = $request->address;
+        $user->phone = $request->phone;
+        $user->save();
+        return redirect()->route('admin.user.add')->with('success', 'Add user successfully');
     }
 }
--- resources/views/backend/user/add.blade.php
+++ resources/views/backend/user/add.blade.php
@@ -5,29 +5,49 @@
             <h3 class="card-title">Quick Example</h3>
         </div>
 
-        <form>
+        <form method="post" action="{{route('admin.user.do-add')}}">
+            @csrf
             <div class="card-body">
                 <div class="form-group">
                     <label for="input-name">Name</label>
                     <input type="text" name="name" class="form-control" id="input-name" placeholder="Enter email">
                 </div>
+
+
+                @error('name')
+                  <div class="text-red">{{$message}}</div>
+                @enderror
+
+
                 <div class="form-group">
                     <label for="input-phone">Phone</label>
                     <input type="text" name="phone" class="form-control" id="input-phone" placeholder="Enter email">
                 </div>
+                @error('phone')
+                <div class="text-red">{{$message}}</div>
+                @enderror
                 <div class="form-group">
                     <label for="input-address">Address</label>
                     <input type="text" name="address" class="form-control" id="input-address" placeholder="Enter email">
                 </div>
+                @error('address')
+                <div class="text-red">{{$message}}</div>
+                @enderror
                 <div class="form-group">
                     <label for="input-email">Email address</label>
                     <input type="email" name="email" class="form-control" id="input-email" placeholder="Enter email">
                 </div>
+                @error('email')
+                <div class="text-red">{{$message}}</div>
+                @enderror
                 <div class="form-group">
                     <label for="input-password">Password</label>
                     <input type="password" name="password" class="form-control" id="input-password"
                            placeholder="Password">
                 </div>
+                @error('password')
+                <div class="text-red">{{$message}}</div>
+                @enderror
                 <div class="form-group">
                     <label for="select-level">Level</label>
                     <select id="select-level" name="level" class="form-control">
@@ -35,6 +55,9 @@
                         <option value="2">User</option>
                     </select>
                 </div>
+                @error('level')
+                <div class="text-red">{{$message}}</div>
+                @enderror
             </div>
 
             <div class="card-footer">
