558f2c7f020d0d5e9a6d7ecc46ab2a24ff84fd91
Nguyen Thanh Luan <luann4099@gmail.com>
2022-12-03 09:42:02 +0700

    add migrate file

---
+++ database/migrations/2022_12_02_041219_create_post_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->string('thumbnail')->nullable();
+            $table->string('slug')->nullable();
+            $table->bigInteger('user_id');
+            $table->bigInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
